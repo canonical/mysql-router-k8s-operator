3150e50443bea68897ddde331fd01f1bbc154de8
Paulo Machado <paulo.machado@canonical.com>
2022-04-05 10:23:20 -0300

    Dpe 134 merge poc into main (#1)
    * General template overwrites
    
    * POC implementation of the charm
    
    * Initial unit and integrations tests
    
    * Add resources and relations
    
    * bump ops version
    
    * Address PR comments
    
    * Remove unused import
    
    * Simpler restart
    
    * Bump year
    
    * run script at container root
    
    * run script at container root

--- src/charm.py
+++ src/charm.py
@@ -1,104 +1,161 @@
 #!/usr/bin/env python3
-# Copyright 2021 Canonical Ltd.
+# Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 #
 # Learn more at: https://juju.is/docs/sdk
 
-"""Charm the service.
+"""MySQL-Router charm."""
 
-Refer to the following post for a quick-start guide that will help you
-develop a new k8s charm using the Operator Framework:
-
-    https://discourse.charmhub.io/t/4208
-"""
-
+import json
 import logging
+from typing import Any
 
 from ops.charm import CharmBase
-from ops.framework import StoredState
 from ops.main import main
-from ops.model import ActiveStatus
+from ops.model import ActiveStatus, WaitingStatus
+from ops.pebble import Layer
 
 logger = logging.getLogger(__name__)
+DATABASE = "database"
+PEER = "mysql-router"
 
 
-class OperatorTemplateCharm(CharmBase):
+class MySQLRouterOperatorCharm(CharmBase):
     """Charm the service."""
-
-    _stored = StoredState()
 
     def __init__(self, *args):
         super().__init__(*args)
-        self.framework.observe(self.on.httpbin_pebble_ready, self._on_httpbin_pebble_ready)
+
+        self.name = "mysqlrouter"
+
+        self.framework.observe(self.on.mysqlrouter_pebble_ready, self._on_mysqlrouter_pebble_ready)
         self.framework.observe(self.on.config_changed, self._on_config_changed)
-        self.framework.observe(self.on.fortune_action, self._on_fortune_action)
-        self._stored.set_default(things=[])
+        self.framework.observe(
+            self.on.database_relation_created, self._on_database_relation_created
+        )
+        self.framework.observe(
+            self.on.database_relation_departed, self._on_database_relation_departed
+        )
 
-    def _on_httpbin_pebble_ready(self, event):
-        """Define and start a workload using the Pebble API.
+    @property
+    def has_db(self) -> bool:
+        """Only consider a DB connection if we have config info."""
+        rel = self.model.get_relation(DATABASE)
+        return len(rel.units) > 0 if rel is not None else False
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        You'll need to specify the right entrypoint and environment
-        configuration for your specific workload. Tip: you can see the
-        standard entrypoint of an existing container using docker inspect
+    @property
+    def peers(self) -> list:
+        """Fetch the peer relation."""
+        return self.model.get_relation(PEER)
 
-        Learn more about Pebble layers at https://github.com/canonical/pebble
-        """
-        # Get a reference the container attribute on the PebbleReadyEvent
-        container = event.workload
-        # Define an initial Pebble layer configuration
-        pebble_layer = {
-            "summary": "httpbin layer",
-            "description": "pebble config layer for httpbin",
-            "services": {
-                "httpbin": {
-                    "override": "replace",
-                    "summary": "httpbin",
-                    "command": "gunicorn -b 0.0.0.0:80 httpbin:app -k gevent",
-                    "startup": "enabled",
-                    "environment": {"thing": self.model.config["thing"]},
-                }
-            },
-        }
+    def set_peer_data(self, key: str, data: Any) -> None:
+        """Put information into the peer data."""
+        if self.unit.is_leader():
+            self.peers.data[self.app][key] = json.dumps(data)
+
+    def get_peer_data(self, key: str) -> Any:
+        """Retrieve information from the peer data."""
+        data = self.peers.data[self.app].get(key, "{}")
+        return json.loads(data)
+
+    def _configure(self) -> None:
+        """Configure the charm."""
+        # Get the relation data
+        data = self.get_peer_data(DATABASE)
+
+        config_data = json.loads(data["mysql"])
+
+        # Define Pebble layer configuration
+        pebble_layer = self._mysqlrouter_layer(
+            port=config_data["port"],
+            host=config_data["host"],
+            user=config_data["user"],
+            password=config_data["password"],
+        )
         # Add initial Pebble config layer using the Pebble API
-        container.add_layer("httpbin", pebble_layer, combine=True)
-        # Autostart any services that were defined with startup: enabled
-        container.autostart()
-        # Learn more about statuses in the SDK docs:
-        # https://juju.is/docs/sdk/constructs#heading--statuses
+        container = self.unit.get_container(self.name)
+        plan = container.get_plan()
+
+        if plan.services != pebble_layer.services:
+            logger.info("Config changed")
+            container.add_layer(self.name, pebble_layer, combine=True)
+
+            container.restart(self.name)
+            logging.info("mysqlrouter restarted")
+
         self.unit.status = ActiveStatus()
 
-    def _on_config_changed(self, _):
-        """Just an example to show how to deal with changed configuration.
+    def _on_mysqlrouter_pebble_ready(self, event) -> None:
+        """Define and start a workload using the Pebble API."""
+        if not self.has_db:
+            logger.debug("No database relation found")
+            self.unit.status = WaitingStatus("Waiting for database relation")
+            event.defer()
+            return
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle config, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the config.py file.
+        self._configure()
 
-        Learn more about config at https://juju.is/docs/sdk/config
+    def _mysqlrouter_layer(self, host, port, user, password) -> dict:
+        """Return a layer configuration for the mysqlrouter service.
+
+        Args:
+            host (str): The hostname of the MySQL cluster.
+            port (int): The port of the MySQL cluster.
+            user (str): The username for the MySQL cluster.
+            password (str): The password for the MySQL cluster.
         """
-        current = self.config["thing"]
-        if current not in self._stored.things:
-            logger.debug("found a new thing: %r", current)
-            self._stored.things.append(current)
+        return Layer(
+            {
+                "summary": "mysqlrouter layer",
+                "description": "pebble config layer for mysqlrouter",
+                "services": {
+                    "mysqlrouter": {
+                        "override": "replace",
+                        "summary": "mysqlrouter",
+                        "command": "/run.sh",
+                        "startup": "enabled",
+                        "environment": {
+                            "MYSQL_PORT": port,
+                            "MYSQL_HOST": host,
+                            "MYSQL_USER": user,
+                            "MYSQL_PASSWORD": password,
+                        },
+                    }
+                },
+            }
+        )
 
-    def _on_fortune_action(self, event):
-        """Just an example to show how to receive actions.
+    def _on_config_changed(self, event) -> None:
+        """Handle config-changed event."""
+        if not self.has_db:
+            logger.debug("No database relation found")
+            self.unit.status = WaitingStatus("Waiting for database relation")
+            event.defer()
+            return
 
-        TEMPLATE-TODO: change this example to suit your needs.
-        If you don't need to handle actions, you can remove this method,
-        the hook created in __init__.py for it, the corresponding test,
-        and the actions.py file.
+        self._configure()
 
-        Learn more about actions at https://juju.is/docs/sdk/actions
-        """
-        fail = event.params["fail"]
-        if fail:
-            event.fail(fail)
-        else:
-            event.set_results({"fortune": "A bug in the code is worth two in the documentation."})
+    def _on_database_relation_created(self, event) -> None:
+        """Handle database relation created event."""
+        logger.info("Database relation created")
+        if not self.has_db:
+            logger.info("No database relation found")
+            self.unit.status = WaitingStatus("Waiting for database relation")
+            event.defer()
+            return
+
+        data = dict(event.relation.data[event.app])
+        self.set_peer_data(DATABASE, data)
+        self._configure()
+
+    def _on_database_relation_departed(self, event) -> None:
+        """Handle database relation departed event."""
+        container = event.workload
+        # Remove the pebble layer
+        container.stop("mysqlrouter")
+
+        self.unit.status = WaitingStatus("Waiting for database relation")
 
 
 if __name__ == "__main__":
-    main(OperatorTemplateCharm)
+    main(MySQLRouterOperatorCharm)
