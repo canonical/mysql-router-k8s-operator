95781a33ac997b7dd6aa796a652b5f0b77476402
Carl Csaposs <carl.csaposs@canonical.com>
2023-10-17 11:49:59 +0000

    300s sleep

--- src/abstract_charm.py
+++ src/abstract_charm.py
@@ -175,8 +175,8 @@ class MySQLRouterCharm(ops.CharmBase, abc.ABC):
     # =======================
 
     def reconcile(self, event=None) -> None:  # noqa: C901
-        if isinstance(event, ops.InstallEvent):
-            time.sleep(60)
+        if isinstance(event, ops.LeaderElectedEvent):
+            time.sleep(300)
         """Handle most events."""
         if not self._upgrade:
             logger.debug("Peer relation not available")
