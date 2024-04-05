bbdfdd6e9f9bfb96e9c4792cebdc3752335b0b0f
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-04-05 16:11:10 +0200

    Set the int. test conditional

--- tests/integration/markers.py
+++ tests/integration/markers.py
@@ -5,7 +5,7 @@ import pytest
 
 from . import juju_
 
-is_3_1_or_higher = pytest.mark.skipif(
-    juju_.is_3_1_or_higher,
+skip_if_lower_than_3_1 = pytest.mark.skipif(
+    not juju_.is_3_1_or_higher,
     reason="Skips juju <3.1.x as we have a dependency for self-signed-certificates",
 )
