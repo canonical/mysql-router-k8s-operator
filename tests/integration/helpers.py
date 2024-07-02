e7f106b19fcd9205f38683f06bdb34ff19c54bbb
Carl Csaposs <carl.csaposs@canonical.com>
2024-07-02 10:00:28 +0200

    Revert "TEMP capture output"
    This reverts commit 5adbacf656e1ab7b40062b940ff4f9839fa34a17.

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -210,23 +210,19 @@ async def write_content_to_file_in_unit(
         temp_file.write(content)
         temp_file.flush()
 
-        try:
-            subprocess.run(
-                [
-                    "microk8s.kubectl",
-                    "cp",
-                    "-n",
-                    ops_test.model.info.name,
-                    "-c",
-                    container_name,
-                    temp_file.name,
-                    f"{pod_name}:{path}",
-                ],
-                check=True,
-                capture_output=True,
-            )
-        except subprocess.CalledProcessError as e:
-            logger.exception(f"{e.stdout=}\n\n{e.stderr=}\n\n")
+        subprocess.run(
+            [
+                "microk8s.kubectl",
+                "cp",
+                "-n",
+                ops_test.model.info.name,
+                "-c",
+                container_name,
+                temp_file.name,
+                f"{pod_name}:{path}",
+            ],
+            check=True,
+        )
 
 
 async def read_contents_from_file_in_unit(
