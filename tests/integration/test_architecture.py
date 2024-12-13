0d2673783c5623c113368d4c9f8202202fbde668
Sinclert Pérez <sinclert.perez@canonical.com>
2024-12-13 11:45:48 +0100

    Include `resources` config into tests

--- tests/integration/test_architecture.py
+++ tests/integration/test_architecture.py
@@ -2,13 +2,17 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+from pathlib import Path
+
 import pytest
+import yaml
 from pytest_operator.plugin import OpsTest
 
 from . import markers
 from .helpers import get_charm
 
-MYSQL_ROUTER_APP_NAME = "mysql-router-k8s"
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
+MYSQL_ROUTER_APP_NAME = METADATA["name"]
 
 
 @pytest.mark.group(1)
@@ -17,10 +21,15 @@ async def test_arm_charm_on_amd_host(ops_test: OpsTest) -> None:
     """Tries deploying an arm64 charm on amd64 host."""
     charm = await get_charm(".", "arm64", 1)
 
+    resources = {
+        "mysql-router-image": METADATA["resources"]["mysql-router-image"]["upstream-source"]
+    }
+
     await ops_test.model.deploy(
         charm,
         application_name=MYSQL_ROUTER_APP_NAME,
         num_units=1,
+        resources=resources,
         base="ubuntu@22.04",
     )
 
@@ -37,10 +46,15 @@ async def test_amd_charm_on_arm_host(ops_test: OpsTest) -> None:
     """Tries deploying an amd64 charm on arm64 host."""
     charm = await get_charm(".", "amd64", 0)
 
+    resources = {
+        "mysql-router-image": METADATA["resources"]["mysql-router-image"]["upstream-source"]
+    }
+
     await ops_test.model.deploy(
         charm,
         application_name=MYSQL_ROUTER_APP_NAME,
         num_units=1,
+        resources=resources,
         base="ubuntu@22.04",
     )
 
