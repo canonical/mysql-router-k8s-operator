a2172071055f3ab870973bc73d1ae0072ec22865
Carl Csaposs <carl.csaposs@canonical.com>
2025-03-19 16:49:22 +0100

    Add refresh v3 draft implementation
    Does not include migration for refreshing from v2 (currently on stable)
    
    This PR branch only supports refreshing to/from mysql-router-k8s charm code with refresh v3

--- src/machine_upgrade.py
+++
@@ -1,8 +0,0 @@
-# Copyright 2023 Canonical Ltd.
-# See LICENSE file for licensing details.
-
-"""Only implemented in machine charm"""
-
-
-class Upgrade:
-    """Only implemented in machine charm"""
